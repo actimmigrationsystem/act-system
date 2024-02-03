e1e4c1cd4c071c394cfdca44c9ef1f9ff8c5a90e
RileyManda <rileymanda69@gmail.com>
2024-02-03 12:12:11 +0200

    Removed test code:ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,6 @@ const FooterSlogan = styled.div`
 
 const CustomFooter = () => (
   <div className="flex flex-col min-h-screen">
-    <div className="flex-grow">Content Layout</div>
     <footer style={{ backgroundColor: '#0e5a97' }} className="bg-white">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
