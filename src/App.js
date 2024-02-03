7ef5321fcceb8ebee1ca7ef6dc22973808906f00
RileyManda <rileymanda69@gmail.com>
2024-02-03 11:10:48 +0200

    Implemeted footer and footer theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar';
 import SocialNav from './components/SocialNav';
+import CustomFooter from './components/Footer';
 
 function App() {
   return (
     <>
       <SocialNav />
       <NavBar />
+      <CustomFooter />
     </>
   );
 }
