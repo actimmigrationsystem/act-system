df10d13137aeafc0ab3581157d3be770c70678a5
RileyManda <rileymanda69@gmail.com>
2024-02-03 12:22:31 +0200

    Hide slogan on small screens:mobile

--- src/components/SocialNav.js
+++ src/components/SocialNav.js
@@ -11,7 +11,7 @@ const SloganText = styled.div`
 export default function SocialNav() {
   return (
     <div className="bg-gray-700 p-2">
-      <SloganText>Your Immigration status is important to us</SloganText>
+      <SloganText className="hidden sm:block md:block lg:block xl:block">Your Immigration status is important to us</SloganText>
       <div className="flex items-center justify-end space-x-4 ml-auto">
         <a href="/" className="text-white hover:text-gray-300" aria-label="Website">
           <FaLinkedin className="h-6 w-6" />
