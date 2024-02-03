6395ac92b621d4a8d1cc6dc218dcf149ad751439
RileyManda <rileymanda69@gmail.com>
2024-02-03 12:09:24 +0200

    Updated hr footer to gray-200

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -63,7 +63,7 @@ const CustomFooter = () => (
             </div>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-200 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-200">
             © 2024
