9ad232991ea38c47473da1791a8c2d09ecd1c9ce
RileyManda <rileymanda69@gmail.com>
2024-02-03 11:58:00 +0200

    Added footer slgan

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
   FaLinkedin, FaWhatsapp, FaInstagram, FaFacebook, FaTwitter,
 } from 'react-icons/fa';
 import { IoMdMail } from 'react-icons/io';
 import Logo from './Logo';
+
+const FooterSlogan = styled.div`
+  font-size: 12px;
+  color: #fff;
+  margin-top: 30px;
+`;
 
 const CustomFooter = () => (
   <div className="flex flex-col min-h-screen">
@@ -14,6 +21,10 @@ const CustomFooter = () => (
           <div className="mb-6 md:mb-0">
 
             <Logo />
+            <FooterSlogan className="text-white">
+              Consultants striving on affecting change in peoples lives everyday.
+              We offer immigration services
+            </FooterSlogan>
 
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -21,21 +32,21 @@ const CustomFooter = () => (
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
               <ul className="text-gray-500 dark:text-gray-200 font-medium">
                 <li className="mb-4">
-                  <a href="https://flowbite.com/" className="hover:underline">ACTImmigration</a>
+                  <a href="/" className="hover:underline">Services</a>
                 </li>
                 <li>
-                  <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                  <a href="/" className="hover:underline">Documents</a>
                 </li>
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Help</h2>
               <ul className="text-gray-500 dark:text-gray-200 font-medium">
                 <li className="mb-4">
-                  <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                  <a href="https://github.com/themesberg/flowbite" className="hover:underline ">FAQs</a>
                 </li>
                 <li>
-                  <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                  <a href="https://www.facebook.com/profile.php?id=100063622943110&ref=embed_page" className="hover:underline">Facebook</a>
                 </li>
               </ul>
             </div>
@@ -57,34 +68,70 @@ const CustomFooter = () => (
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-200">
             © 2024
             {' '}
-            <a href="https://flowbite.com/" className="hover:underline">ACT Immigration</a>
+            <a href="/" className="hover:underline">ACT IMMIGRATION AND LABOUR CONSULTANTS</a>
             . All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
-            <button type="button" className="text-gray-500 hover:text-gray-900 dark:hover:text-white" onClick={() => { }}>
-              <FaLinkedin className="w-4 h-4 text-white" />
-              <span className="sr-only">Linkedin</span>
+
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <a href="https://www.facebook.com/profile.php?id=100063622943110&ref=embed_page" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin className="w-4 h-4 text-white" />
+                <span className="sr-only">Facebook</span>
+              </a>
             </button>
-            <button type="button" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5" onClick={() => { }}>
-              <FaFacebook className="w-4 h-4 text-white" />
-              <span className="sr-only">Discord community</span>
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <a href="https://www.facebook.com/profile.php?id=100063622943110&ref=embed_page" target="_blank" rel="noopener noreferrer">
+                <FaFacebook className="w-4 h-4 text-white" />
+                <span className="sr-only">Facebook</span>
+              </a>
             </button>
-            <button type="button" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5" onClick={() => { }}>
-              <FaWhatsapp className="w-4 h-4 text-white" />
-              <span className="sr-only">Twitter page</span>
+
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <a href="https://api.whatsapp.com/send?phone=27723876910" target="_blank" rel="noopener noreferrer">
+                <FaWhatsapp className="w-4 h-4 text-white" />
+                <span className="sr-only">Facebook</span>
+              </a>
             </button>
-            <button type="button" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5" onClick={() => { }}>
-              <FaTwitter className="w-4 h-4 text-white" />
-              <span className="sr-only">GitHub account</span>
+
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <a href="https://www.facebook.com/profile.php?id=100063622943110&ref=embed_page" target="_blank" rel="noopener noreferrer">
+                <FaTwitter className="w-4 h-4 text-white" />
+                <span className="sr-only">Facebook</span>
+              </a>
             </button>
-            <button type="button" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5" onClick={() => { }}>
-              <FaInstagram className="w-4 h-4 text-white" />
-              <span className="sr-only">Dribbble account</span>
+
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <a href="https://www.facebook.com/profile.php?id=100063622943110&ref=embed_page" target="_blank" rel="noopener noreferrer">
+                <FaInstagram className="w-4 h-4 text-white" />
+                <span className="sr-only">Facebook</span>
+              </a>
             </button>
-            <button type="button" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5" onClick={() => { }}>
-              <IoMdMail className="w-4 h-4 text-white" />
-              <span className="sr-only">Dribbble account</span>
+
+            <button
+              type="button"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <a href="https://www.facebook.com/profile.php?id=100063622943110&ref=embed_page" target="_blank" rel="noopener noreferrer">
+                <IoMdMail className="w-4 h-4 text-white" />
+                <span className="sr-only">Facebook</span>
+              </a>
             </button>
+
           </div>
         </div>
       </div>
