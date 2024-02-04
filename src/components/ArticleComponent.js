5a480b2b538cf53dc553470305ae711db2cda265
RileyManda <rileymanda69@gmail.com>
2024-02-05 00:08:12 +0200

    Hotfix:Article layout

--- src/components/ArticleComponent.js
+++ src/components/ArticleComponent.js
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types';
 
 function ArticleComponent({
   heading, lead, subheading, subheading2, paragraph,
-  paragraph2,
+  paragraph2, imageSrc,
 }) {
   return (
-    <article className="format lg:format-lg">
-      <h1 className="text-2xl">{heading}</h1>
-      <p className="lead">{lead}</p>
-      <h2 className="text-1xl">{subheading}</h2>
-      <p>{paragraph}</p>
-      <h2 className="text-1xl">{subheading2}</h2>
-      <p>{paragraph2}</p>
-    </article>
+    <div className="p-2">
+      <article className="">
+        {imageSrc && <img src={imageSrc} alt="Article" className="w-20 object-cover object-top rounded-md mr-4" />}
+        <div>
+          <h2 className="text-2xl font-bold mb-4">{heading}</h2>
+          <p className="text-lg text-gray-600 mb-6">{lead}</p>
+          <h2 className="text-2xl font-bold mb-2">{subheading}</h2>
+          <p className="text-lg text-gray-700 mb-4">{paragraph}</p>
+          {subheading2 && <h2 className="text-2xl font-bold mb-2">{subheading2}</h2>}
+          {paragraph2 && <p className="text-lg text-gray-700 mb-4">{paragraph2}</p>}
+        </div>
+      </article>
+    </div>
   );
 }
 
@@ -25,11 +30,13 @@ ArticleComponent.propTypes = {
   subheading2: PropTypes.string,
   paragraph: PropTypes.string.isRequired,
   paragraph2: PropTypes.string,
+  imageSrc: PropTypes.string,
 };
 
 ArticleComponent.defaultProps = {
   subheading2: '',
   paragraph2: '',
+  imageSrc: '',
 };
 
 export default ArticleComponent;
