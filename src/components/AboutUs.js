ed79c2ea6277be7d25a288420213dfbddf260819
RileyManda <rileymanda69@gmail.com>
2024-02-04 22:52:38 +0200

    Card and Typography enhancement:AboutUs|Service Card component|Added flowbite article dependency

---
+++ src/components/AboutUs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import SectionTitle from './SectionTitle';
+
+const AboutUs = () => (
+  <div className="">
+    <SectionTitle
+      title="About Us"
+    />
+    <p>Who we are</p>
+    <p className="mb-3 font-normal text-base  text-gray-700 dark:text-gray-400">
+      We are a team of legal practitioners and consultants,
+      specialising in Immigration and Labour-Related matters.
+
+      As a practicing attorney for over a decade, and graduating
+      from the Nelson Mandela Metropolitan University, our Director
+      is an Immigration Law specialist who has changed thousands of
+      lives in the labour and immigration avenue.
+
+      Our Mission
+      Our mission is to help every client with an immigration or
+      labour-related issue efficiently, while yielding nothing but satisfaction for our clients.
+
+      We use our legal background,and innate grit, to fight for the rights of our clients.
+
+      Contact us, book an appointment, and prepare for your life to be changed.
+    </p>
+  </div>
+);
+export default AboutUs;
--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,25 +1,46 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 // ServiceCard.jsx
 import PropTypes from 'prop-types';
-import { Card, Button } from 'flowbite-react';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from '@material-tailwind/react';
 
 const ServiceCard = ({ image, title, description }) => (
-  <div className="p-8 mt-3 pt-4">
-    <Card className="mt-6 mb-6 w-96">
-      <div className="relative h-56">
-        <img
-          className="w-full h-full object-cover"
-          src={image}
-          alt="card image"
-        />
-      </div>
-      <div className="p-4">
-        <h5 className="text-2xl font-bold mb-2 text-gray-900">{title}</h5>
-        <p className="text-gray-700 dark:text-gray-900">{description}</p>
-      </div>
-      <div className="pt-0">
-        <Button>Read More</Button>
-      </div>
+  <div className="w-full flex flex-col sm:flex-row">
+    <Card className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-200 dark:border-gray-200 relative overflow-hidden">
+      <CardBody className="p-2">
+        <img className="rounded-t-lg" src={image} alt="" />
+        <a href="/">
+          <Typography className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+            {title}
+          </Typography>
+        </a>
+        <p className="mb-3 font-normal text-base  text-gray-700 dark:text-gray-400">{description}</p>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <a href="/" className="inline-block">
+          <Button size="sm" variant="text" className="flex items-center gap-2">
+            Learn More
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </Button>
+        </a>
+      </CardFooter>
     </Card>
   </div>
 );
