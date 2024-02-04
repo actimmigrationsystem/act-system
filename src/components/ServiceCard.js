0254b3eb241a0726422ff5d6de3d4f005bb6051e
RileyManda <rileymanda69@gmail.com>
2024-02-04 18:51:51 +0200

    Updated card style:shadow"Realigned cards content and typography

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,16 +1,23 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+// ServiceCard.jsx
 import PropTypes from 'prop-types';
-import { Card } from 'flowbite-react';
+import { Card, Button } from 'flowbite-react';
 
 const ServiceCard = ({ image, title, description }) => (
-  <Card className="max-w-fit text-gray-900 rounded-lg shadow overflow-hidden">
-    <img
-      className="w-full h-48 object-cover object-center"
-      src={image}
-      alt={title}
-    />
-    <div className="p-4 flex flex-col items-center justify-center h-full">
-      <h5 className="text-1xl font-bold tracking-tight text-gray-900">{title}</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-900">{description}</p>
+  <Card className="mt-6 mb-6 w-96">
+    <div className="relative h-56">
+      <img
+        className="w-full h-full object-cover"
+        src={image}
+        alt="card image"
+      />
+    </div>
+    <div className="p-4">
+      <h5 className="text-2xl font-bold mb-2 text-gray-900">{title}</h5>
+      <p className="text-gray-700 dark:text-gray-900">{description}</p>
+    </div>
+    <div className="pt-0">
+      <Button>Read More</Button>
     </div>
   </Card>
 );
