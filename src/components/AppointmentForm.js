c87825e32cd20985e2d40763d8c197fc0b13a501
RileyManda <rileymanda69@gmail.com>
2024-02-07 00:52:40 +0200

    Enhancement:Data component

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
-import { Typography, Button } from '@material-tailwind/react';
+import {
+  Typography, Button, MenuItem,
+  Select,
+} from '@material-tailwind/react';
 import { FloatingLabel } from 'flowbite-react';
+import DateComponent from './DateComponents';
 
 const AppointmentForm = () => {
   const [name, setName] = useState('');
   const [phonenumber, setPhonenumber] = useState('');
   const [query, setQuery] = useState('');
   const [contactEmail, setContactEmail] = useState('');
+  const [category] = useState('');
 
   return (
     <div>
       <form
-        action="https://formsubmit.co/rileymanda0@gmail.com"
+        action="https://formsubmit.co/david@actimmigration.co.za"
         method="POST"
         className="mt-12 flex flex-col gap-4"
       >
@@ -98,6 +103,38 @@ const AppointmentForm = () => {
               className: 'before:content-none after:content-none',
             }}
           />
+        </div>
+        <div>
+          <Typography
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-medium"
+          >
+            Select Date
+          </Typography>
+          <DateComponent />
+        </div>
+        <div>
+          <Typography
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-medium"
+          >
+            Select Category For your appointment
+          </Typography>
+          <Select
+            value={category}
+            displayEmpty
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+          >
+            <MenuItem value="" disabled>
+              Select Service Type
+            </MenuItem>
+            <MenuItem value="appeals">Appeals</MenuItem>
+            <MenuItem value="commision of oaths">Commision of Oaths</MenuItem>
+            <MenuItem value="visaspermits">Visa & Permits</MenuItem>
+            <MenuItem value="other">Other</MenuItem>
+          </Select>
         </div>
         <Button
           btnlabel="Submit Enquiry"
