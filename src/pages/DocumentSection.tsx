b7a256af43d49399683fb7d5ab2e3353d312393d
RileyManda <rileymanda69@gmail.com>
2024-02-08 23:54:57 +0200

    Updated margins top and bottom faq and documents section

--- src/pages/DocumentSection.tsx
+++ src/pages/DocumentSection.tsx
@@ -5,7 +5,7 @@ import SectionContainer from '../components/SectionContainer';
 
 export default function DocumentSection() {
   return (
-    <SectionContainer height="70vh" marginTop="15px" marginBottom="40px">
+    <SectionContainer height="70vh" marginTop="15px" marginBottom="50px">
       <SectionTitle title="Documents" className="text-center text-white" />
       <div className="bg-gray-700 p-2">
         <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
