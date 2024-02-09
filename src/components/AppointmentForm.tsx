04db332210e1133ee9ccb31f09269ec978bf8ed7
RileyManda <rileymanda69@gmail.com>
2024-02-09 22:45:29 +0200

    Implemented step form-Enquiry

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'react';
-import {
-  Typography, Button, MenuItem,
-  Select,
-} from '@material-tailwind/react';
-import { FloatingLabel } from 'flowbite-react';
-import DateComponent from './DateComponents';
+import { useState } from "react";
+import { Typography, Button, MenuItem, Select } from "@material-tailwind/react";
+import { FloatingLabel } from "flowbite-react";
+import DateComponent from "./DateComponents";
 
 const AppointmentForm = () => {
-  const [name, setName] = useState('');
-  const [phonenumber, setPhonenumber] = useState('');
-  const [query, setQuery] = useState('');
-  const [contactEmail, setContactEmail] = useState('');
-  const [category] = useState('');
+  const [name, setName] = useState("");
+  const [phonenumber, setPhonenumber] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+  const [category] = useState("");
+  const [service, setService] = useState("");
+  const [immigrationStatus, setImmigrationStatus] = useState("");
+  const [previousAdvice, setPreviousAdvice] = useState("");
+  const [nationality, setNationality] = useState("");
+  const [serviceType, setServiceType] = useState("");
 
   return (
     <div>
@@ -27,7 +28,7 @@ const AppointmentForm = () => {
             color="blue-gray"
             className="mb-2 font-medium"
           >
-            Name
+            Full Name
           </Typography>
           <FloatingLabel
             variant="filled"
@@ -65,25 +66,6 @@ const AppointmentForm = () => {
             color="blue-gray"
             className="mb-2 font-medium"
           >
-            Query
-          </Typography>
-          <FloatingLabel
-            variant="filled"
-            label="Your query....."
-            type="textarea"
-            name="query"
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
-          />
-        </div>
-        <div>
-          <Typography
-            placeholder={"Typography"}
-            variant="small"
-            color="blue-gray"
-            className="mb-2 font-medium"
-          >
             Contact Email
           </Typography>
           <FloatingLabel
@@ -95,6 +77,166 @@ const AppointmentForm = () => {
             onChange={(event) => setContactEmail(event.target.value)}
             className="!border-t-blue-gray-200 focus:!border-t-gray-900"
           />
+        </div>
+        <div>
+          <Typography
+            placeholder={"Typography"}
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-medium"
+          >
+            Which service do you require?
+          </Typography>
+          <Select
+            placeholder={"Select"}
+            value={service}
+            onChange={(value) => {
+              if (value) {
+                setService(value);
+              }
+            }}
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+          >
+            <MenuItem placeholder="" value="" disabled>
+              Visa
+            </MenuItem>
+            <MenuItem placeholder="service1" value="service1">
+              Permits
+            </MenuItem>
+            <MenuItem placeholder="service2" value="service2">
+              Appeals
+            </MenuItem>
+            <MenuItem placeholder="service3" value="service3">
+             Legal Assistance(Other)
+            </MenuItem>
+          </Select>
+        </div>
+
+        <div>
+          <Typography
+            placeholder={"Typography"}
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-medium"
+          >
+            What is your current immigration status?
+          </Typography>
+          <Select
+            placeholder={"Select"}
+            value={immigrationStatus}
+            onChange={(value) => {
+              if (value) {
+                setImmigrationStatus(value);
+              }
+            }}
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+          >
+            <MenuItem placeholder="" value="" disabled>
+             No Status
+            </MenuItem>
+            <MenuItem placeholder="service1" value="service1">
+              Work Visa
+            </MenuItem>
+            <MenuItem placeholder="service2" value="service2">
+              Permanent Resident
+            </MenuItem>
+            <MenuItem placeholder="service3" value="service3">
+              Asylum
+            </MenuItem>
+          </Select>
+        </div>
+
+        <div>
+          <Typography
+            placeholder={"Typography"}
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-medium"
+          >
+            Have you received previous advice on this before?
+          </Typography>
+          <Select
+            placeholder={"Select"}
+            value={previousAdvice}
+            onChange={(value) => {
+              if (value) {
+                setPreviousAdvice(value);
+              }
+            }}
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+          >
+            <MenuItem placeholder="" value="yes" disabled>
+             Yes
+            </MenuItem>
+            <MenuItem placeholder="No" value="no">
+              No
+            </MenuItem>
+          </Select>
+        </div>
+
+        <div>
+          <Typography
+            placeholder={"Typography"}
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-medium"
+          >
+            What is your nationality?
+          </Typography>
+          <Select
+            placeholder={"Select"}
+            value={nationality}
+            onChange={(value) => {
+              if (value) {
+                setNationality(value);
+              }
+            }}
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+          >
+            <MenuItem placeholder="" value="" disabled>
+              Zimbabwe
+            </MenuItem>
+            <MenuItem placeholder="service1" value="service1">
+              Kenya
+            </MenuItem>
+            <MenuItem placeholder="service2" value="service2">
+              Ehtiopia
+            </MenuItem>
+          </Select>
+        </div>
+
+        <div>
+          <Typography
+            placeholder={"Typography"}
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-medium"
+          >
+            How would you like to receive this service?
+          </Typography>
+          <Select
+            placeholder={"Select"}
+            value={serviceType}
+            onChange={(value) => {
+              if (value) {
+                setServiceType(value);
+              }
+            }}
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+          >
+            <MenuItem placeholder="" value="" disabled>
+              In Person-Office
+            </MenuItem>
+            <MenuItem placeholder="service1" value="service1">
+              Remote(Zoom)
+            </MenuItem>
+            <MenuItem placeholder="service2" value="service2">
+              Email
+            </MenuItem>
+            <MenuItem placeholder="service3" value="service3">
+              Phonecall
+            </MenuItem>
+          </Select>
         </div>
         <div>
           <Typography
@@ -117,7 +259,7 @@ const AppointmentForm = () => {
             Select Category For your appointment
           </Typography>
           <Select
-          placeholder={"Select"}
+            placeholder={"Select"}
             value={category}
             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
           >
@@ -127,15 +269,19 @@ const AppointmentForm = () => {
             <MenuItem placeholder="appeals" value="appeals">
               Appeals
             </MenuItem>
-            <MenuItem placeholder="appeals"  value="commision of oaths">
+            <MenuItem placeholder="appeals" value="commision of oaths">
               Commision of Oaths
             </MenuItem>
-            <MenuItem placeholder="appeals" value="visaspermits">Visa & Permits</MenuItem>
-            <MenuItem placeholder="appeals" value="other">Other</MenuItem>
+            <MenuItem placeholder="appeals" value="visaspermits">
+              Visa & Permits
+            </MenuItem>
+            <MenuItem placeholder="appeals" value="other">
+              Other
+            </MenuItem>
           </Select>
         </div>
         <Button
-        placeholder={"Button"}
+          placeholder={"Button"}
           size="lg"
           type="submit"
           style={{ backgroundColor: "#0e5a97" }}
--- src/pages/ContactUsSection.tsx
+++ src/pages/ContactUsSection.tsx
@@ -1,20 +1,17 @@
 import SectionTitle from '../components/SectionTitle';
-import ContactForm from '../components/ContactCard';
+import ContactCard from '../components/ContactCard';
 import MapComponent from '../components/MapComponent';
 import ContentContainer from '../components/ContentContainer';
 import SectionContainer from '../components/SectionContainer';
 
 const ContactUsSection = () => (
-  <div style={{ backgroundColor: '#2393CB00' }}>
+  <div style={{ backgroundColor: "#2393CB00" }}>
     <SectionContainer height="90vh" marginTop="90px" marginBottom="20px">
-      <SectionTitle
-        title="Contact Us"
-        className="text-center text-white"
-      />
+      <SectionTitle title="Contact Us" className="text-center text-white" />
       <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <ContentContainer>
           <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2">
-            <ContactForm />
+            <ContactCard />
             <MapComponent />
           </div>
         </ContentContainer>
