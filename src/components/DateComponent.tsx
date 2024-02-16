6ac9f4d29a8687be2aeebc2e4c536444b96549f9
RileyManda <rileymanda69@gmail.com>
2024-02-16 13:52:38 +0200

    Updated appintment form

--- src/components/DateComponent.tsx
+++ src/components/DateComponent.tsx
@@ -1,20 +1,12 @@
-import React from "react";
+"use client";
+
 import { Datepicker } from "flowbite-react";
 
-const DateComponent = ({
-  appointmentDate,
-  setAppointmentDate,
-}: {
-  appointmentDate: string;
-  setAppointmentDate: React.Dispatch<React.SetStateAction<string>>;
-}) => (
-  <div>
+const DateComponent = () => {
+  return (
     <Datepicker
-      value={appointmentDate}
-      onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAppointmentDate(event.target.value)}
-      className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+      weekStart={1} // Monday
     />
-  </div>
-);
-
+  );
+}
 export default DateComponent;
