0dbafa6f17bf5baa97cc4561ffd676e30a32f2f4
RileyManda <rileymanda69@gmail.com>
2024-02-17 16:14:45 +0200

    Implemented additional hours to Timepicker

--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -14,6 +14,10 @@ const times: string[] = [
   "09:30 AM",
   "10:00 AM",
   "10:30 AM",
+  "11:00 AM",
+  "11:30 AM",
+  "12:00 AM",
+  "12:30 AM",
 ];
 
 const TimePicker: React.FC<TimePickerProps> = ({ value, onChange }) => {
