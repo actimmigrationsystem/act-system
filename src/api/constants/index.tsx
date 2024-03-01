cdd3f61ee5a09318e4a1b9a25ae3445d180e5be9
RileyManda <rileymanda69@gmail.com>
2024-03-01 16:26:47 +0200

    Extracted form data into api data array

--- src/api/constants/index.tsx
+++ src/api/constants/index.tsx
@@ -1,36 +1,47 @@
+import Img1 from "../../assets/services/1.jpg";
+import Img3 from "../../assets/services/3.jpg";
+import Img7 from "../../assets/services/7.jpeg";
+import Img6 from "../../assets/services/6.png";
+import Img5 from "../../assets/services/5.jpeg";
+
 export const FAQ_DATA = [
   {
-    "id": 1,
-    "question": "How much do your legal services cost?",
-    "answer": "Our fees vary based on the complexity of the case and the services required. We offer initial consultations to discuss your case and provide an estimate of the costs involved."
+    id: 1,
+    question: "How much do your legal services cost?",
+    answer:
+      "Our fees vary based on the complexity of the case and the services required. We offer initial consultations to discuss your case and provide an estimate of the costs involved.",
   },
   {
-    "id": 3,
-    "question": "How can I schedule a consultation?",
-    "answer": "To schedule a consultation, you can call our office or send us a whatsapp or fill in the contact form and we will revert back as soon as possible to schedule a convenient time for you."
+    id: 3,
+    question: "How can I schedule a consultation?",
+    answer:
+      "To schedule a consultation, you can call our office or send us a whatsapp or fill in the contact form and we will revert back as soon as possible to schedule a convenient time for you.",
   },
   {
-    "id": 4,
-    "question": "What information should I bring to the initial consultation?",
-    "answer": "For the initial consultation, it would be helpful to bring any relevant documents. This will assist our lawyers in understanding your case and providing tailored advice."
+    id: 4,
+    question: "What information should I bring to the initial consultation?",
+    answer:
+      "For the initial consultation, it would be helpful to bring any relevant documents. This will assist our lawyers in understanding your case and providing tailored advice.",
   },
   {
-    "id": 5,
-    "question": "What types of visas do you handle?",
-    "answer": "We handle a variety of visas, including temporary and permanent residence visas, as well as specific types such as general work visas."
+    id: 5,
+    question: "What types of visas do you handle?",
+    answer:
+      "We handle a variety of visas, including temporary and permanent residence visas, as well as specific types such as general work visas.",
   },
   {
-    "id": 6,
-    "question": "Can you assist with appeals and reviews?",
-    "answer": "Yes, we can assist with appeals and reviews in terms of the immigration act. An aggrieved applicant may submit representations in the form of an appeal or review."
+    id: 6,
+    question: "Can you assist with appeals and reviews?",
+    answer:
+      "Yes, we can assist with appeals and reviews in terms of the immigration act. An aggrieved applicant may submit representations in the form of an appeal or review.",
   },
   {
-    "id": 7,
-    "question": "How do I renew a temporary residence visa?",
-    "answer": "The temporary residence visa must be renewed while the permanent residence application is still pending. Our team can guide you through the process."
-  }
+    id: 7,
+    question: "How do I renew a temporary residence visa?",
+    answer:
+      "The temporary residence visa must be renewed while the permanent residence application is still pending. Our team can guide you through the process.",
+  },
 ];
-
 
 export const privacypolicydata = [
   {
@@ -38,5 +49,38 @@ export const privacypolicydata = [
     title: "Introduction",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque lacinia eros vel auctor. Donec vel mollis magna. Integer a",
+  },
+];
+
+export const serviceData = [
+  {
+    image: Img6,
+    title: "Temporary and permanent residence visas",
+    description:
+      "We offer assistance with temporary and permanent residence visa applications as well as SCRA reviews and appeals.",
+  },
+  {
+    image: Img5,
+    title: "SCRA reviews and appeals",
+    description:
+      "Our SCRA reviews and appeals services are designed to help you efficiently navigate the process while ensuring that your rights are protected.",
+  },
+  {
+    image: Img1,
+    title: "Appeals and reviews in terms of the immigration act",
+    description:
+      "We provide comprehensive support for appeals and reviews under the Immigration Act.",
+  },
+  {
+    image: Img7,
+    title: "Types of temporary residence visas",
+    description:
+      "Our team is experienced in handling various types of temporary residence visas, including general work visas.",
+  },
+  {
+    image: Img3,
+    title: "Permanent residence permit applications",
+    description:
+      "We offer assistance with permanent residence permit applications, ensuring that your  visa is renewed.",
   },
 ];
--- src/pages/ServicesSection.tsx
+++ src/pages/ServicesSection.tsx
@@ -1,46 +1,10 @@
 import SectionTitle from "../components/SectionTitle";
 import ServiceCard from "../components/ServiceCard";
-import Img1 from "../assets/services/1.jpg";
-import Img3 from "../assets/services/3.jpg";
-import Img4 from "../assets/services/4.jpg";
-import Img6 from "../assets/services/6.png";
-import Img5 from "../assets/services/5.jpeg";
 import SectionContainer from "../components/SectionContainer";
 import ContentContainer from "../components/ContentContainer";
 import { Link } from "react-router-dom";
+import { serviceData } from "../api/constants";
 
-const serviceData = [
-  {
-    image: Img6,
-    title: "Temporary and permanent residence visas",
-    description:
-      "We offer assistance with temporary and permanent residence visa applications as well as SCRA reviews and appeals.",
-  },
-  {
-    image: Img5,
-    title: "SCRA reviews and appeals",
-    description:
-      "Our SCRA reviews and appeals services are designed to help you efficiently navigate the process while ensuring that your rights are protected.",
-  },
-  {
-    image: Img1,
-    title: "Appeals and reviews in terms of the immigration act",
-    description:
-      "We provide comprehensive support for appeals and reviews under the Immigration Act.",
-  },
-  {
-    image: Img4,
-    title: "Types of temporary residence visas",
-    description:
-      "Our team is experienced in handling various types of temporary residence visas, including general work visas.",
-  },
-  {
-    image: Img3,
-    title: "Permanent residence permit applications",
-    description:
-      "We offer assistance with permanent residence permit applications, ensuring that your  visa is renewed.",
-  },
-];
 
 const ServicesSection = () => (
   <SectionContainer>
