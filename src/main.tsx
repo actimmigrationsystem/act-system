15d8df70d8e703e9e6055a9a3617ae15797e4e1c
RileyManda <rileymanda69@gmail.com>
2024-03-14 11:05:34 +0200

    Implemented appolo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphiql',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </ApolloProvider>
 );
