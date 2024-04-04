de0c0655e5560632558c60a995252ae56a59ae58
RileyManda <rileymanda69@gmail.com>
2024-04-04 19:48:19 +0200

    Updated default layout to cdnter content in view(flex)| mt-16

--- src/pages/dashboard/DashBoardLayout.tsx
+++ src/pages/dashboard/DashBoardLayout.tsx
@@ -9,10 +9,12 @@ interface DashboardLayoutProps {
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="flex h-screen overflow-hidden">
-      <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden">
+      <div className="w-64 flex-shrink-0">
+        <Sidebar />
+      </div>
+      <div className="flex flex-col flex-1">
         <DashboardNavBar />
-        <main className="flex-1 p-5 overflow-y-auto bg-gray-100 dark:bg-gray-800 flex items-center justify-end pt-40">
+        <main className="flex-1 p-5 overflow-y-auto bg-gray-100 dark:bg-gray-800 mt-16">
           {children}
         </main>
       </div>
