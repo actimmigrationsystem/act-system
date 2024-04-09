a6caad10d719be2a3a25ad3c9d7546813dc9a861
RileyManda <rileymanda69@gmail.com>
2024-04-09 13:06:14 +0200

    Updated breadcrumb margins and children top margin

--- src/pages/dashboard/DashBoardLayout.tsx
+++ src/pages/dashboard/DashBoardLayout.tsx
@@ -14,12 +14,12 @@ const DashboardLayout = ({ children, pageTitle }: DashboardLayoutProps) => {
       <SideBar />
       <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         <DashboardNavBar />
-        <header className="px-2 sm:px-6 lg:px-8 py-4">
+
+        <main className="flex-1 mt-20 px-2 sm:px-6 lg:px-8 py-4">
           <div className="max-w-full">
             <Breadcrumb pageName={pageTitle} />
           </div>
-        </header>
-        <main className="flex-1">
+
           <div className="px-4 sm:px-6 lg:px-8 py-8 max-w-full">{children}</div>
         </main>
       </div>
