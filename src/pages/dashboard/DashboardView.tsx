da5ff518f1b5a4c206933e569c40624ebfaca4d0
RileyManda <rileymanda69@gmail.com>
2024-04-10 23:33:11 +0200

    Implemented display enquiries

--- src/pages/dashboard/DashboardView.tsx
+++ src/pages/dashboard/DashboardView.tsx
@@ -8,7 +8,19 @@ const enquiryRoute = import.meta.env.VITE_ENQUIRY_ROUTE;
 interface Enquiry {
   id: number;
   name: string;
+  phonenumber: string;
+  email: string;
+  gender: string;
+  dob: string;
+  maritalStatus: string;
+  residentialAddress: string;
+  immigrationStatus: string;
+  entryDate: string;
+  passportNumber: string;
+  referenceNumber: string;
+  documentUpload: string;
   serviceType: string;
+  elaborate: string;
 }
 
 const DashboardView = () => {
@@ -50,12 +62,134 @@ useEffect(() => {
         <h1 className="text-3xl font-bold text-gray-800 dark:text-white">
           Welcome to your Dashboard, {email}
         </h1>
-        {enquiries.map((enquiry) => (
-          <div key={enquiry.id}>
-            <h2>{enquiry.name}</h2>
-            <p>{enquiry.serviceType}</p>
+        <div className="grid grid-cols-3 gap-4 mt-10">
+          <div className="col-span-1"></div>
+          <div className="col-span-2">
+            {enquiries.map((enquiry) => (
+              <div
+                key={enquiry.id}
+                className="bg-white shadow overflow-hidden sm:rounded-lg mb-4"
+              >
+                <div className="px-4 py-5 sm:px-6">
+                  <h2 className="text-lg leading-6 font-medium text-gray-900">
+                    {enquiry.name}
+                  </h2>
+                </div>
+                <div className="border-t border-gray-200">
+                  <dl>
+                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Service Type
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.serviceType}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Phone Number
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.phonenumber}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Email
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.email}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Gender
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.gender}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Date Of Birth
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.dob}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Date Of Birth
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.maritalStatus}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Residential Address
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.residentialAddress}
+                      </dd>
+                    </div>
+
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Residential Address
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.immigrationStatus}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Residential Address
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.entryDate}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Residential Address
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.passportNumber}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Residential Address
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.referenceNumber}
+                      </dd>
+                    </div>
+
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Documents
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.documentUpload}
+                      </dd>
+                    </div>
+
+                    <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        Elaboration on service requirements
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                        {enquiry.elaborate}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </DashboardLayout>
   );
